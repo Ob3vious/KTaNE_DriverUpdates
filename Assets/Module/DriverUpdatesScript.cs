a84abd4616e8f5772f3f57990c146f3b2d6ab632
GhostSalt <mhanna756@c2ken.net>
2025-06-24 22:41:19 +0100

    -Downsize the grid to 8×8

--- Assets/Module/DriverUpdatesScript.cs
+++ Assets/Module/DriverUpdatesScript.cs
@@ -15,29 +15,19 @@ public class DriverUpdatesScript : MonoBehaviour
         GetComponent<KMBombModule>().OnActivate += delegate
         {
             Marquee.AssignTexts(new string[] { "THIS IS EXAMPLEEEEEEE|TEXTYY WEXTYyyyyyy", "h", "ababababababababababababababababababababa", "", "wheeee|bobm" });
-            GridRend.SetLED(5, 6, true);
-            GridRend.SetLED(5, 5, true);
-            GridRend.SetLED(6, 6, true);
-            GridRend.SetLED(6, 5, true);
-            GridRend.SetLED(5, 9, true);
-            GridRend.SetLED(5, 10, true);
-            GridRend.SetLED(6, 9, true);
-            GridRend.SetLED(6, 10, true);
-            GridRend.SetLED(11, 7, true);
-            GridRend.SetLED(11, 6, true);
-            GridRend.SetLED(11, 5, true);
-            GridRend.SetLED(10, 4, true);
-            GridRend.SetLED(11, 8, true);
-            GridRend.SetLED(11, 9, true);
-            GridRend.SetLED(11, 10, true);
-            GridRend.SetLED(10, 11, true);
+            GridRend.SetLED(2, 2, true);
+            GridRend.SetLED(2, 5, true);
+            GridRend.SetLED(4, 2, true);
+            GridRend.SetLED(4, 5, true);
+            GridRend.SetLED(5, 3, true);
+            GridRend.SetLED(5, 4, true);
         };
 
         DriverStoragePuzzle puzzle = new DriverStoragePuzzle(8, 8);
 
         //20, 19, 11, 6
 
-        puzzle = puzzle.FindPuzzle(new List<int>() { 12, 7, 6, 4 });
-        Debug.Log(Enumerable.Range(0, 8).Select(y => Enumerable.Range(0, 8).Select(x => puzzle.Grid[y, x] ? "#" : ".").Join("")).Join(";"));
+        //puzzle = puzzle.FindPuzzle(new List<int>() { 12, 7, 6, 4 });
+        //Debug.Log(Enumerable.Range(0, 8).Select(y => Enumerable.Range(0, 8).Select(x => puzzle.Grid[y, x] ? "#" : ".").Join("")).Join(";"));
     }
 }
