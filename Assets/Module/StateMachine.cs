4db1e0b76e03565cb266e48bf302aa29a9036e47
Ob3vious <niels.leest04@gmail.com>
2025-06-29 14:25:52 +0200

    what the fuck is UnityEditorInternal

--- Assets/Module/StateMachine.cs
+++ Assets/Module/StateMachine.cs
@@ -2,10 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Threading;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class UpdaterStateMachine
